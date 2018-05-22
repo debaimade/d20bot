c2e38a66b94f86e76a31e888f57436d7f36e3506
d.imade <debaimade@users.noreply.github.com>
2018-05-22 13:54:51 -0500

    Initial commit

---
+++ d20bot/d20.py
@@ -0,0 +1,32 @@
+'''
+A simple discord bot made to roll a 20 sided die. 
+Built and modified from the tutorials at devdungeon
+https://www.devdungeon.com/content/make-discord-bot-python
+'''
+import discord
+import random
+
+TOKEN = 'NDQ4NTM3NDEzNjM2MTk0MzM0.DeXktQ.2tN85t6zoh4OHrLqWujHAOUBJ2U'
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!roll'):
+        side = random.randint(1,20)
+        response = 'You rolled a ' + str(side)
+        msg = response.format(message)
+        await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)
