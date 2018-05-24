0cee030067664487f21eb565ad5454d9e0cf8b4a
Michael Karr <fxfrax@gmail.com>
2018-05-24 14:56:24 -0500

    Add fortune command.
    Add a fortune command and the BSD fortune files to provide a random
    fortune upon request.

--- d20bot/d20.py
+++ d20bot/d20.py
@@ -4,6 +4,7 @@ Built and modified from the tutorials at devdungeon
 https://www.devdungeon.com/content/make-discord-bot-python
 '''
 import discord
+import os
 import random
 
 client = discord.Client()
@@ -17,6 +18,17 @@ async def on_message(message):
     if message.content.startswith('!roll'):
         side = random.randint(1,20)
         response = 'You rolled a ' + str(side)
+        msg = response.format(message)
+        await client.send_message(message.channel, msg)
+    elif message.content.startswith('!fortune'):
+        fortunes_dir = 'fortune'
+        fortunes = []
+        
+        for fortune_file in os.listdir(fortunes_dir):
+            fortune_fh = open(os.path.join(fortunes_dir, fortune_file), 'r')
+            fortunes.extend(fortune_fh.read().split("\n%\n"))
+        
+        response = random.choice(fortunes)
         msg = response.format(message)
         await client.send_message(message.channel, msg)
 
